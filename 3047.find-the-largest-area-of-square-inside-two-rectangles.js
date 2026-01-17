5601edecd4ac775b0a1ed3556b9c00f36353b614
Froggles <elmsterm@gmail.com>
2026-01-16 23:34:58 -0500

    start problem

---
+++ 3047.find-the-largest-area-of-square-inside-two-rectangles.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3047 lang=javascript
+ *
+ * [3047] Find the Largest Area of Square Inside Two Rectangles
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} bottomLeft
+ * @param {number[][]} topRight
+ * @return {number}
+ */
+var largestSquareArea = function (bottomLeft, topRight) {};
+// @lc code=end
